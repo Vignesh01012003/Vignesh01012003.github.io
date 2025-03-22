09365eed71bf4dc680859c47bdd05ff78fc2970b
Vignesh01012003 <146185295+Vignesh01012003@users.noreply.github.com>
2025-03-23 05:02:23 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("changeColor").addEventListener("click", function() {
+    document.body.style.backgroundColor = 
+        document.body.style.backgroundColor === "lightblue" ? "#f4f4f4" : "lightblue";
+});
